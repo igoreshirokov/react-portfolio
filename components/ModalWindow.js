3a5bacbf1e0a72574878d265927da7ec11bd232e
igoreshirokov <igoreshirokov@yandex.ru>
2021-09-13 09:02:24 +0300

    &

--- components/ModalWindow.js
+++ components/ModalWindow.js
@@ -1,7 +1,16 @@
-export default function ModalWindow({ children }) {
+export default function ModalWindow({ children, functionClose }) {
     return (
         <div className="modal-window">
+            <div className="modal-header">
+                <div className="vlinecenter"></div>
+            </div>
+            <div onClick={functionClose} className="button-close"></div>
+
             {children}
+
+            <div className="modal-footer">
+                <div className="footer-vline"></div>
+            </div>
         </div>
     )
 }
--- pages/services.js
+++ pages/services.js
@@ -2,19 +2,28 @@ import styles from '../styles/services.module.sass';
 import MainLayout from "../layouts/MainLayout";
 import { useState } from "react";
 import ModalWindow from '../components/ModalWindow';
+import { Development, Design, Scrapping} from '../components/services/ServicesDescriptions';
+
 
 export default function Services() {
     const [modal, setModal] = useState(true)
 
-    function openModal() {
-        console.log('click')
+    function openModal(content) {
+        setModal(content)
     }
+    function closeModal() {
+        setModal(false)
+    }
+
+
+
+
 
     return (
         <div>
 
-            {modal ? <ModalWindow>
-                modalwindow
+            {modal ? <ModalWindow functionClose={closeModal}>
+                {modal}
             </ModalWindow> : <MainLayout title="Услуги">
 
                 <div className={styles.services}>
@@ -26,17 +35,17 @@ export default function Services() {
                         <div className={`${styles.servicesListCard} ${styles.servicesListCardOdd}`}>
                             <h3>Разработка</h3>
                             <p>У вас есть дизайн? Отлично, верстка готового макета, программирование модулей, установка CMS и прочее.</p>
-                            <button className="light-button" onClick={() => openModal()}>Подробнее</button>
+                            <button className="light-button" onClick={() => openModal(Development)}>Подробнее</button>
                         </div>
                         <div className={styles.servicesListCard}>
                             <h3>Сбор данных</h3>
                             <p>Парсинг/скрапинг - обработка контента, например, каталог интернет-магазина, статьи болга, обработка и предоставление в удобном виде.</p>
-                            <button className="light-button" onClick={() => openModal()}>Подробнее</button>
+                            <button className="light-button" onClick={() => openModal(Scrapping)}>Подробнее</button>
                         </div>
                         <div className={styles.servicesListCard}>
                             <h3>Дизайн</h3>
                             <p>Разрабатываю сайт под ключ, включая дизайн, верстку и установку на хостинг.</p>
-                            <button className="light-button" onClick={() => openModal()}>Подробнее</button>
+                            <button className="light-button" onClick={() => openModal(Design)}>Подробнее</button>
                         </div>
                     </div>
                 </div>
